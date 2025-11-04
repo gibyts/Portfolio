cac510a023065aa89ce751fc77ba40757d60656f
Gibyts <gibrantarrillo1604@gmail.com>
2025-11-04 15:17:43 -0500

    fix: ajustar el valor de 'amount' en el viewport para mejorar la visibilidad en la sección

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -7,7 +7,7 @@ export default function Section({ children, className = "", id }) {
       initial={{ opacity: 0, y: 50 }}
       whileInView={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.8, ease: "easeOut" }}
-      viewport={{ once: false, amount: 0.2 }}
+      viewport={{ once: false, amount: 0.1 }}
       className={className} // ✅ Tus clases originales
     >
       {children}
